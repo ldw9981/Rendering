e3f7c81fe36ea97e3651f31d1df61b6b3d355dc2
ldw9981 <ldw9981@093e8ea8-f476-11dd-87c9-111d11f6bf44>
2010-01-25 01:09:07 +0000

    리팩토링

--- trunk/D3DEngine/D3DEngine/Framework/StaticD3DDevice9.cpp
+++
@@ -1,4 +0,0 @@
-#include "stdafx.h"
-#include "StaticD3DDevice9.h"
-
-LPDIRECT3DDEVICE9 cStaticD3DDEVICE9::m_pD3DDevice=NULL;
--- trunk/D3DEngine/D3DEngine/Framework/StaticD3DDevice9.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class cStaticD3DDEVICE9
-{
-public:
-	cStaticD3DDEVICE9(void) {};
-	~cStaticD3DDEVICE9(void) {};
-protected:
-	static LPDIRECT3DDEVICE9	m_pD3DDevice;
-};

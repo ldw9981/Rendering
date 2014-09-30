b845f5bbb86d019f05fa84059882ed09b8ce4736
ldw9981 <ldw9981@gmail.com>
2014-09-30 23:33:18 +0000

    material 종류에따른 technique 사이즈 자동계산 1차수정

--- src/Graphics/Graphics.cpp
+++ src/Graphics/Graphics.cpp
@@ -30,11 +30,6 @@ LPDIRECT3DDEVICE9 Graphics::m_pDevice;
 Graphics::Graphics(void)
 {
 	m_pInstance = this;
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
-	{	
-		m_hTNormal[i] = NULL;
-		m_hTBlend[i] = NULL;
-	}
 	m_viewPortInfo.X = 0;
 	m_viewPortInfo.Y = 0;
 	m_viewPortInfo.Width = 1024;
@@ -45,10 +40,21 @@ Graphics::Graphics(void)
 	m_pShadowRenderTarget = NULL;
 	m_pShadowDepthStencil = NULL;
 	m_bDebugBound = false;
+
+	m_nTechniqueSize = (int)pow(2.0f,Material::MAX);
+	m_pTNormal = new D3DXHANDLE[m_nTechniqueSize];
+	m_pTBlend = new D3DXHANDLE[m_nTechniqueSize];
+	for (int i=0;i<m_nTechniqueSize;i++)
+	{	
+		m_pTNormal[i] = NULL;
+		m_pTBlend[i] = NULL;
+	}
 }
 
 Graphics::~Graphics(void)
 {
+	delete[] m_pTNormal;
+	delete[] m_pTBlend;
 }
 
 void Graphics::SetPos( UINT x,UINT y )
@@ -234,53 +240,56 @@ void Graphics::LoadHLSL(const char* szFileName)
 
 	std::bitset<Material::MAX> indexRenderQueue;
 
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhong;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhong;
 
 	indexRenderQueue.set(Material::DIFFUSE);
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuse;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuse;
 
 	indexRenderQueue = 0;
 	indexRenderQueue.set(Material::DIFFUSE);
 	indexRenderQueue.set(Material::NORMAL);
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseBump;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseBump;
 
 	indexRenderQueue = 0;
 	indexRenderQueue.set(Material::DIFFUSE);
 	indexRenderQueue.set(Material::LIGHT);
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseLight;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseLight;
 
 	indexRenderQueue = 0;
 	indexRenderQueue.set(Material::DIFFUSE);
 	indexRenderQueue.set(Material::OPACITY);
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseOpacity;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseOpacity;
 
 	indexRenderQueue = 0;
 	indexRenderQueue.set(Material::DIFFUSE);
 	indexRenderQueue.set(Material::SPECULAR);
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseSpecular;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseSpecular;
 
 	indexRenderQueue = 0;
 	indexRenderQueue.set(Material::DIFFUSE);
 	indexRenderQueue.set(Material::NORMAL);
 	indexRenderQueue.set(Material::SPECULAR);
-	m_hTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseBumpSpecular;
+	m_pTNormal[indexRenderQueue.to_ulong()] = m_hTPhongDiffuseBumpSpecular;
 
 
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
+	for (int i=0;i<m_nTechniqueSize;i++)
 	{	
-		if (m_hTNormal[i] == NULL )	
-			m_hTNormal[i] = m_hTPhongDiffuse;
+		if (m_pTNormal[i] == NULL )	
+			m_pTNormal[i] = m_hTPhongDiffuse;
 	}
 
-	indexRenderQueue.reset();
-	m_hTBlend[indexRenderQueue.to_ulong()] = m_hTSkinningPhong;
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
+	indexRenderQueue = 0;
+	m_pTBlend[indexRenderQueue.to_ulong()] = m_hTSkinningPhong;
+
+	indexRenderQueue = 0;
+	indexRenderQueue.set(Material::DIFFUSE);
+	m_pTBlend[indexRenderQueue.to_ulong()] = m_hTSkinningPhongDiffuse;
+
+	for (int i=0;i<m_nTechniqueSize;i++)
 	{	
-		if (m_hTBlend[i] == NULL )	
-			m_hTBlend[i] = m_hTSkinningPhongDiffuse;
+		if (m_pTBlend[i] == NULL )	
+			m_pTBlend[i] = m_hTSkinningPhongDiffuse;
 	}
-
-
 }
 
 
@@ -295,304 +304,6 @@ void Graphics::End()
 {
 	m_pDevice->EndScene();
 	m_pDevice->Present( NULL, NULL, m_hWndPresent, NULL );	
-}
-
-void Graphics::RenderEX( World* pWorld )
-{
-	cCameraNode* pCamera = &pWorld->m_camera;
-
-	UINT passes = 0;
-	m_pDevice->SetViewport(&pWorld->m_ViewPortInfo);
-
-	// 광원-뷰 행렬을 만든다.
-	D3DXMATRIX matLightView;
-	{
-		D3DXVECTOR3 vEyePt( pWorld->m_WorldLightPosition.x, pWorld->m_WorldLightPosition.y,  pWorld->m_WorldLightPosition.z );		
-		D3DXVECTOR3 vLookatPt;
-		pCamera->GetWorldPos(vLookatPt);
-
-		vEyePt += vLookatPt;
-		D3DXVECTOR3 vUpVec(    0.0f, 1.0f,  0.0f );
-		D3DXMatrixLookAtLH( &matLightView, &vEyePt, &vLookatPt, &vUpVec );
-	}
-
-	// 광원-투영 행렬을 만든다.
-	D3DXMATRIX matLightProjection;
-	{
-		//D3DXMatrixPerspectiveFovLH( &matLightProjection, D3DX_PI / 4.0f, 1, 3000, FAR_PLANE );
-		D3DXMatrixOrthoLH( &matLightProjection, SHADOWMAP_SIZE,SHADOWMAP_SIZE, NEAR_PLANE, FAR_PLANE );
-	}
-
-
-	// 현재 하드웨어 벡버퍼와 깊이버퍼
-	LPDIRECT3DSURFACE9 pHWBackBuffer = NULL;
-	LPDIRECT3DSURFACE9 pHWDepthStencilBuffer = NULL;
-	m_pDevice->GetRenderTarget(0, &pHWBackBuffer);
-	m_pDevice->GetDepthStencilSurface(&pHWDepthStencilBuffer);
-
-	//////////////////////////////
-	// 1. 그림자 만들기
-	//////////////////////////////
-
-	// 그림자 맵의 렌더타깃과 깊이버퍼를 사용한다.
-	LPDIRECT3DSURFACE9 pShadowSurface = NULL;
-	if( SUCCEEDED( m_pShadowRenderTarget->GetSurfaceLevel( 0, &pShadowSurface ) ) )
-	{
-		m_pDevice->SetRenderTarget( 0, pShadowSurface );
-		pShadowSurface->Release();
-		pShadowSurface = NULL;
-	}
-	m_pDevice->SetDepthStencilSurface( m_pShadowDepthStencil );
-	m_pDevice->Clear( 0, NULL, (D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER), 0xFFFFFFFF, 1.0f, 0 );
-
-	m_pEffect->SetVector(m_hvWorldLightPosition,&pWorld->m_WorldLightPosition);
-	m_pEffect->SetMatrix(m_hmLightView, &matLightView);
-	m_pEffect->SetMatrix(m_hmLightProjection, &matLightProjection);	
-
-
-	//1. write depth
-	m_pEffect->SetTechnique(m_hTCreateShadowNormal);
-	m_pEffect->Begin(&passes, 0);
-	m_pEffect->BeginPass(0);
-	pWorld->m_renderQueueNormalShadow.Render();
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-	m_pEffect->SetTechnique(m_hTCreateShadowBlend);
-	m_pEffect->Begin(&passes, 0);
-	m_pEffect->BeginPass(0);
-	pWorld->m_renderQueueSkinnedShadow.Render();
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-
-
-	//////////////////////////////
-	// 2. 그림자 입히기
-	//////////////////////////////
-
-	// 하드웨어 백버퍼/깊이버퍼를 사용한다.
-	m_pDevice->SetRenderTarget( 0, pHWBackBuffer );
-	m_pDevice->SetDepthStencilSurface(pHWDepthStencilBuffer);
-
-	pHWBackBuffer->Release();
-	pHWBackBuffer = NULL;
-	pHWDepthStencilBuffer->Release();
-	pHWDepthStencilBuffer = NULL;
-
-	m_pEffect->SetTexture("ShadowMap_Tex", m_pShadowRenderTarget);
-
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
-	{
-		m_pEffect->SetTechnique(m_hTNormal[i]);
-		m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-		m_pEffect->BeginPass(0);	
-		pWorld->m_renderQueueNormal[i].Render();		
-		m_pEffect->EndPass();
-		m_pEffect->End();
-	}
-
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
-	{
-		m_pEffect->SetTechnique(m_hTBlend[i]);
-		m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-		m_pEffect->BeginPass(0);	
-		pWorld->m_renderQueueSkinned[i].Render();
-		m_pEffect->EndPass();
-		m_pEffect->End();
-	}
-
-	m_pEffect->SetTechnique(m_hTerrain);
-	m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-	m_pEffect->BeginPass(0);	
-	pWorld->m_renderQueueTerrain.Render();
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-	
-	if (m_bDebugBound)
-	{
-		m_pEffect->SetTechnique(m_hTLine);
-		m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-		m_pEffect->BeginPass(0);
-		for ( auto itEntityRender = pWorld->m_listEntityRender.begin() ;itEntityRender != pWorld->m_listEntityRender.end() ; ++itEntityRender )
-		{
-			(*itEntityRender)->RenderBound();
-		}	
-		m_pEffect->EndPass();
-		m_pEffect->End();	
-	}
-	
-	m_pDevice->SetTexture (0, m_pShadowRenderTarget );
-	m_pDevice->SetFVF(FVF_GUIVERTEX);
-	m_pDevice->DrawPrimitiveUP( D3DPT_TRIANGLEFAN, 2, &g_vertices[0], sizeof(GUIVERTEX));
-}
-
-void Graphics::Render( World* pWorld )
-{
-	m_pDevice->SetViewport(&pWorld->m_ViewPortInfo);
-	cCameraNode* pCamera = &pWorld->m_camera;
-	UINT passes = 0;
-	// 광원-뷰 행렬을 만든다.
-	D3DXMATRIX matLightView;
-	{
-		D3DXVECTOR3 vEyePt( pWorld->m_WorldLightPosition.x, pWorld->m_WorldLightPosition.y,  pWorld->m_WorldLightPosition.z );		
-		D3DXVECTOR3 vLookatPt;
-		pCamera->GetWorldPos(vLookatPt);
-
-		vEyePt += vLookatPt;
-		D3DXVECTOR3 vUpVec(    0.0f, 1.0f,  0.0f );
-		D3DXMatrixLookAtLH( &matLightView, &vEyePt, &vLookatPt, &vUpVec );
-	}
-
-	// 광원-투영 행렬을 만든다.
-	D3DXMATRIX matLightProjection;
-	{
-		//D3DXMatrixPerspectiveFovLH( &matLightProjection, D3DX_PI / 4.0f, 1, 3000, FAR_PLANE );
-		D3DXMatrixOrthoLH( &matLightProjection, SHADOWMAP_SIZE,SHADOWMAP_SIZE, NEAR_PLANE, FAR_PLANE );
-	}
-
-
-	// 현재 하드웨어 벡버퍼와 깊이버퍼
-	LPDIRECT3DSURFACE9 pHWBackBuffer = NULL;
-	LPDIRECT3DSURFACE9 pHWDepthStencilBuffer = NULL;
-	m_pDevice->GetRenderTarget(0, &pHWBackBuffer);
-	m_pDevice->GetDepthStencilSurface(&pHWDepthStencilBuffer);
-
-	//////////////////////////////
-	// 1. 그림자 만들기
-	//////////////////////////////
-
-	// 그림자 맵의 렌더타깃과 깊이버퍼를 사용한다.
-	LPDIRECT3DSURFACE9 pShadowSurface = NULL;
-	if( SUCCEEDED( m_pShadowRenderTarget->GetSurfaceLevel( 0, &pShadowSurface ) ) )
-	{
-		m_pDevice->SetRenderTarget( 0, pShadowSurface );
-		pShadowSurface->Release();
-		pShadowSurface = NULL;
-	}
-	m_pDevice->SetDepthStencilSurface( m_pShadowDepthStencil );
-	m_pDevice->Clear( 0, NULL, (D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER), 0xFFFFFFFF, 1.0f, 0 );
-
-	m_pEffect->SetVector(m_hvWorldLightPosition,&pWorld->m_WorldLightPosition);
-	m_pEffect->SetMatrix(m_hmLightView, &matLightView);
-	m_pEffect->SetMatrix(m_hmLightProjection, &matLightProjection);	
-
-
-	//1. write depth
-	m_pEffect->SetTechnique(m_hTCreateShadowNormal);
-	m_pEffect->Begin(&passes, 0);
-	m_pEffect->BeginPass(0);
-	auto itEntityShadow = pWorld->m_listEntityShadow.begin();
-	for ( ;itEntityShadow != pWorld->m_listEntityShadow.end() ; ++itEntityShadow )
-	{
-		(*itEntityShadow)->m_renderQueueNormalShadow.Render();
-	}		
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-	m_pEffect->SetTechnique(m_hTCreateShadowBlend);
-	m_pEffect->Begin(&passes, 0);
-	m_pEffect->BeginPass(0);
-	itEntityShadow = pWorld->m_listEntityShadow.begin();
-	for ( ;itEntityShadow != pWorld->m_listEntityShadow.end() ; ++itEntityShadow )
-	{
-		(*itEntityShadow)->m_renderQueueSkinnedShadow.Render();
-	}	
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-
-
-
-	//////////////////////////////
-	// 2. 그림자 입히기
-	//////////////////////////////
-
-	// 하드웨어 백버퍼/깊이버퍼를 사용한다.
-	m_pDevice->SetRenderTarget( 0, pHWBackBuffer );
-	m_pDevice->SetDepthStencilSurface(pHWDepthStencilBuffer);
-
-	pHWBackBuffer->Release();
-	pHWBackBuffer = NULL;
-	pHWDepthStencilBuffer->Release();
-	pHWDepthStencilBuffer = NULL;
-
-	m_pEffect->SetTexture("ShadowMap_Tex", m_pShadowRenderTarget);
-
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
-	{
-		m_pEffect->SetTechnique(m_hTNormal[i]);
-		m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-		m_pEffect->BeginPass(0);	
-
-		auto itEntityRender = pWorld->m_listEntityRender.begin();
-		for ( ;itEntityRender != pWorld->m_listEntityRender.end() ; ++itEntityRender )
-		{
-			(*itEntityRender)->m_renderQueueNormal[i].Render();
-		}	
-
-		m_pEffect->EndPass();
-		m_pEffect->End();
-	}
-
-	for (int i=0;i<TECHNIQUE_SIZE;i++)
-	{
-		m_pEffect->SetTechnique(m_hTBlend[i]);
-		m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-		m_pEffect->BeginPass(0);	
-
-		auto itEntityRender = pWorld->m_listEntityRender.begin();
-		for ( ;itEntityRender != pWorld->m_listEntityRender.end() ; ++itEntityRender )
-		{
-			(*itEntityRender)->m_renderQueueSkinned[i].Render();
-		}	
-
-		m_pEffect->EndPass();
-		m_pEffect->End();
-	}
-
-	m_pEffect->SetTechnique(m_hTerrain);
-	m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-	m_pEffect->BeginPass(0);	
-
-	auto itEntityRender = pWorld->m_listEntityRender.begin();
-	for ( ;itEntityRender != pWorld->m_listEntityRender.end() ; ++itEntityRender )
-	{
-		(*itEntityRender)->m_renderQueueTerrain.Render();
-	}	
-
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-	if (m_bDebugBound)
-	{
-		m_pEffect->SetTechnique(m_hTLine);
-		m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-		m_pEffect->BeginPass(0);
-		itEntityRender = pWorld->m_listEntityRender.begin();
-		for ( ;itEntityRender != pWorld->m_listEntityRender.end() ; ++itEntityRender )
-		{
-			(*itEntityRender)->RenderBound();
-		}	
-		m_pEffect->EndPass();
-		m_pEffect->End();	
-	}
-
-	m_pEffect->SetTechnique(m_hTGUI);
-	m_pEffect->Begin(&passes, 0);	// 쉐이더 설정은 꼭 Begin전에 한다. 따라서 쉐이더별로 정렬이 필요하다
-	m_pEffect->BeginPass(0);
-	for ( auto it = pWorld->m_mapButton.begin();it != pWorld->m_mapButton.end() ; ++it )
-	{
-		(*it).second->Render();
-	}	
-	m_pEffect->EndPass();
-	m_pEffect->End();
-
-	// SHADOW_MAP
-	m_pDevice->SetTexture (0, m_pShadowRenderTarget );
-	m_pDevice->SetFVF(FVF_GUIVERTEX);
-	m_pDevice->DrawPrimitiveUP( D3DPT_TRIANGLEFAN, 2, &g_vertices[0], sizeof(GUIVERTEX));
 }
 
 void Graphics::SetEffectVector_WorldLightPosition(D3DXVECTOR4* pVec )

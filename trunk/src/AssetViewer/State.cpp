75296d88190438121869309b695e19c3f4d3f0b1
ldw9981@gmail.com <ldw9981@gmail.com@093e8ea8-f476-11dd-87c9-111d11f6bf44>
2014-10-01 17:50:21 +0000

    viewer에서 프로파일링은 적절치 않음

--- trunk/src/AssetViewer/State.cpp
+++ trunk/src/AssetViewer/State.cpp
@@ -32,7 +32,7 @@ State::State(void)
 	m_bModifiedScene = false;
 	m_bModifiedMaterial = false;	
 	m_scrollSize = 100.0f;
-	m_nStressCount = 100;
+	m_nStressCount = 0;
 }
 
 State::~State( void )
@@ -197,9 +197,6 @@ void State::OpenAsset( const char* path )
 	m_pModel->Build();
 	m_pModel->SetParentNode(&m_helper);
 	D3DXMatrixIdentity(&m_helper.GetLocalTM());
-
-	ClearStressEntity();
-	OpenStressEntity(path);
 }
 
 Sophia::Entity* State::GetEntity()

3b6d853999ff7bbaeefaadba7dfbca03b8a6e98a
ldw9981@gmail.com <ldw9981@gmail.com@093e8ea8-f476-11dd-87c9-111d11f6bf44>
2014-10-18 09:26:09 +0000

    인스턴싱관련 리소스추가

---
+++ trunk/src/Graphics/BoneStreamTexture.cpp
@@ -0,0 +1,36 @@
+#include "StdAfx.h"
+#include "BoneStreamTexture.h"
+#include "Resource/ResourceMng.h"
+#include "Foundation/Define.h"
+
+using namespace Sophia;
+BoneStreamTexture::BoneStreamTexture(void)
+{
+	m_width = 256*4;
+	m_height = 128;
+	m_levels = 1;
+	m_pool = D3DPOOL_DEFAULT;
+	m_usage = D3DUSAGE_DYNAMIC;
+	m_format = D3DFMT_A32B32G32R32F ;
+
+	m_key.pIndexBuffer = NULL;
+	m_key.pMaterial = NULL;
+	m_key.pVertexBuffer = NULL;
+	m_valid = false;
+}
+
+
+BoneStreamTexture::~BoneStreamTexture(void)
+{
+}
+
+void BoneStreamTexture::Free()
+{
+	SAFE_RELEASE(m_pD3DTexture);
+
+	if (m_key.pIndexBuffer != NULL && m_key.pMaterial!=NULL && m_key.pVertexBuffer!=NULL)
+	{
+		cResourceMng::m_pInstance->EraseBoneStreamTexture(m_key);
+	}	
+	delete this;
+}

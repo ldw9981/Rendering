bbd689d559b86e11e319c84b08b33a8f061778d7
ldw9981 <ldw9981@gmail.com>
2014-12-27 06:26:29 +0000

    실행파일 출력및 경로 설정 변경

--- src/Foundation/EnvironmentVariable.cpp
+++ src/Foundation/EnvironmentVariable.cpp
@@ -29,4 +29,28 @@ EnvironmentVariable& EnvironmentVariable::GetInstance()
 	static EnvironmentVariable instance;
 	return instance;
 }
+
+void EnvironmentVariable::SetPath()
+{
+	char szModuleFolder[1024] = { 0, };
+	char* pszTemp;
+	GetModuleFileName(GetModuleHandle(NULL), szModuleFolder, 1023);
+
+	pszTemp = strrchr(szModuleFolder, '\\');
+	if(pszTemp) {
+		*pszTemp = 0;
+	}	
+	EnvironmentVariable::GetInstance().SetString("BinPath",std::string(szModuleFolder)+std::string("\\"));
+
+
+	pszTemp = strrchr(szModuleFolder, '\\');
+	if(pszTemp) {
+		*pszTemp = 0;
+	}	
+	EnvironmentVariable::GetInstance().SetString("BasePath",std::string(szModuleFolder)+std::string("\\"));
+
+
+	EnvironmentVariable::GetInstance().SetString("DataPath",std::string(szModuleFolder)+std::string("\\Data\\"));
+}
+
 }
